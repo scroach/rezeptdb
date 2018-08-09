f83ef72076f164a8799825d6cbb129d03d4fabf3
ShiNoSenshi <darksenshi@gmail.com>
2018-08-09 20:52:00 +0200

    added fos_rest composer requirements

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
