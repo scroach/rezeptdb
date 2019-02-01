8563a3bb37ae34c19ca20620811877d2f3f5ec30
scroach <patrick.winkelmayer@gmail.com>
2019-02-01 01:05:40 +0100

    extracted css and js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,14 @@
 $(function () {
 	$('.ui.dropdown').dropdown();
+
 	$('.menu .ui.dropdown').dropdown({on: 'hover'});
+
+	$('#toggle-sidebar-button').click(function () {
+		$('.ui.sidebar').sidebar('toggle');
+	});
+
+	$("#gallery").unitegallery({
+		gallery_theme: "compact",
+		theme_panel_position: "right"
+	});
 });
